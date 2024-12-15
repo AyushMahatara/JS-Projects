607436061c19dd55c4d1714ea0785e495b325ca6
Ayush Mahatara <ayush.mahatara@islingtoncollege.edu.np>
2024-12-15 21:12:13 +0545

    random color

--- 1/app.js
+++ 1/app.js
@@ -1,1 +1,22 @@
 const body = document.getElementById("body");
+const green = document.getElementById("green");
+const blue = document.getElementById("blue");
+const red = document.getElementById("red");
+const random = document.getElementById("random");
+
+green.addEventListener("click", () => {
+  body.style.backgroundColor = "green";
+});
+
+blue.addEventListener("click", () => {
+  body.style.backgroundColor = "blue";
+});
+
+red.addEventListener("click", () => {
+  body.style.backgroundColor = "red";
+});
+
+random.addEventListener("click", () => {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  body.style.backgroundColor = "#" + randomColor;
+});
